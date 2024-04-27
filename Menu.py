b6056c8bfaee1d088ceab1e4ee1c32cf44823c2a
zverkovboris <32186297+zverkovboris@users.noreply.github.com>
2024-04-27 14:17:41 +0300

    Start page update

--- Menu.py
+++ Menu.py
@@ -1,9 +1,35 @@
 import streamlit as st
+import json
+
 
 st.set_page_config(
     page_title="Event Tracking Dashboard",
     page_icon="🏂",
     layout="wide",
     initial_sidebar_state="expanded")
+st.markdown("# Select venue parameters")
 
-st.title('Aalto AI Geeks Space Analytics Tool')
+col = st.columns((3,3), gap="medium")
+with col[0]:
+    st.markdown('### Tracking details')
+    people = st.toggle("People")
+    bottle = st.toggle("Bottles")
+
+with col[1]:
+    st.markdown("### Venue details")
+    size = st.number_input("Size (m^2)", value=100)
+    max_capacity = st.number_input("Maximum Attendance", value=50)
+
+
+start = st.button("Start")
+
+if start:
+    data = {
+        "people": people,
+        "bottle": bottle,
+        "size": size,
+        "max_capacity": max_capacity
+    }
+    with open("parameters.json", "w") as f:
+        json.dump(data, f)
+    st.switch_page("./pages/Video_tracking.py")
