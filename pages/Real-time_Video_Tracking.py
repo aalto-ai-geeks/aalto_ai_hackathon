dcd284028eb717d6e7ca811c93f0ee2f361a4507
zverkovboris <32186297+zverkovboris@users.noreply.github.com>
2024-04-27 14:25:23 +0300

    Update video

--- pages/Real-time_Video_Tracking.py
+++ pages/Real-time_Video_Tracking.py
@@ -1,15 +1,43 @@
 import streamlit as st
 import altair as alt
 import cv2
+import cv2
+import socket
+import pickle
+import struct
+import json
 
+# Create a socket client
+
+
+from ultralytics import YOLO
+
+model = YOLO('yolov8s-world.pt')
+model = model.to("cuda")
+
+with open("parameters.json", "r") as f:
+    data = json.load(f)
+
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+classNames = []
+
+
+if data["people"]:
+    classNames.append("person")
+if data["bottle"]:
+    classNames.append("bottle")
+
+
+model.set_classes(classNames)
 
 def main():
 
     alt.themes.enable("dark")
 
-    rtsp_url = "rtsp://smartbi:aigeeks@192.168.28.243:554/stream1"
-
-    # Sidebar
+    rtsp_url = f'rtsp://{config["user"]}:{config["pass"]}@{config["ip"]}:{config["port"]}/stream1'
 
     camera = cv2.VideoCapture(rtsp_url, cv2.CAP_FFMPEG)
 
@@ -18,29 +46,14 @@ def main():
         st.text("This view enables online tracking of the venue.")
 
     st.markdown('#### Event scene')
-
-    option = st.selectbox(
-        'What would you like to track?',
-        ('People', 'Tables', 'Food'))
-
-    # PLACEHOLDER the option should be passed to YOLO model
-    st.write(option)
-
     FRAME_WINDOW = st.image([])
-    _, frame = camera.read()
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # run = st.checkbox('Stream')
-    # # if run:
-    FRAME_WINDOW.image(img)
-    stream = st.button('Stream')
-    if stream:
-        while True:
-            _, frame = camera.read()
-            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # run = st.checkbox('Stream')
-            # # if run:
-            FRAME_WINDOW.image(img)
 
+    while True:
+        _, frame = camera.read()
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = model.track(img, persist=True, show=False, device="cuda")
+        annotated_frame = results[0].plot()
+        FRAME_WINDOW.image(annotated_frame)
 
 if __name__ == "__main__":
     main()
